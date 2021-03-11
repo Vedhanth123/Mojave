284abb83ff93381a9ced2331b2cabc03c29cbf14
Vedhanth <gvedhanth123@outlook.com>
2021-03-11 12:01:25 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
-﻿using System;
+// equivalent to #include in c and c++. All the basic files]
+// required are defined in System
+using System;
 
+// Creating a namespace
 namespace c_sharp
 {
+    
+    // Classes are defined in C# programming
     class Program
     {
+        
+        // Classes have functions and this is the main function in C#
         static void Main(string[] args)
         {
+            // equivalent to printf in C and C++
             Console.WriteLine("Hello World!");
         }
     }
